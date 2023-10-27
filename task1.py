2b5c8b49667535c8cd4a53791005062e6b7a50ea
PolitovAS <razreztv@gmail.com>
2023-10-27 23:33:07 +0500

    upd task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+N = 10
+sum = 0
+for i in range(1, N + 1):
+    sum += i
+print("Сумма чисел от 1 до", N, "равна", sum)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+numbers = [4, 2, 7, 1, 9, 3]
+sorted_numbers = []
+while numbers:
+    min_num = min(numbers)
+    sorted_numbers.append(min_num)
+    numbers.remove(min_num)
+print("Отсортированный список:", sorted_numbers)
 
+def longest_common_subsequence(str1, str2):
+    m, n = len(str1), len(str2)
+    # Создаем двумерный массив для хранения длин НОП для подстрок str1 и str2.
+    lcs_lengths = [[0] * (n + 1) for _ in range(m + 1)]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Заполняем массив lcs_lengths в соответствии с алгоритмом динамического программирования.
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                lcs_lengths[i][j] = lcs_lengths[i - 1][j - 1] + 1
+            else:
+                lcs_lengths[i][j] = max(lcs_lengths[i - 1][j], lcs_lengths[i][j - 1])
 
+    # Теперь восстанавливаем саму НОП, начиная с конца массива lcs_lengths.
+    lcs = []
+    i, j = m, n
+    while i > 0 and j > 0:
+        if str1[i - 1] == str2[j - 1]:
+            lcs.append(str1[i - 1])
+            i -= 1
+            j -= 1
+        elif lcs_lengths[i - 1][j] > lcs_lengths[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Инвертируем список, чтобы получить НОП в правильном порядке.
+    lcs = lcs[::-1]
+    return "".join(lcs)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Пример использования:
+str1 = "AGGTAB"
+str2 = "GXTXAYB"
+result = longest_common_subsequence(str1, str2)
+print("Наибольшая общая подпоследовательность:", result)
